b7eafaac13975573f5eb3bef944271ab2cb56dc3
SuvroGfz <1905020@ugrad.cse.buet.ac.bd>
2024-09-21 22:07:14 +0600

    patch

--- src/main/java/dev/sabri/securityjwt/config/SecurityConfiguration.java
+++ src/main/java/dev/sabri/securityjwt/config/SecurityConfiguration.java
@@ -164,7 +164,7 @@ public class SecurityConfiguration {
 //        configuration.setAllowedHeaders(List.of("*"));
 
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // or your specific frontend host
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
         configuration.setAllowCredentials(true); // Important for cookies, authorization headers with HTTPS
 
