f1ddb0fb8bee89290bb0473840937e87437024e5
SuvroGfz <1905020@ugrad.cse.buet.ac.bd>
2024-09-22 13:02:03 +0600

    hosting changes p2

--- src/main/java/dev/sabri/securityjwt/config/CorsConfig.java
+++ src/main/java/dev/sabri/securityjwt/config/CorsConfig.java
@@ -1,20 +1,20 @@
-package dev.sabri.securityjwt.config;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-public class CorsConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // Apply CORS to all endpoints
-                        .allowedOrigins("http://localhost:3000") // Allow only your frontend origin
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow necessary HTTP methods
-                        .allowedHeaders("*") // Allow all headers
-                        .allowCredentials(true); // Allow credentials (e.g., cookies, authorization headers)
-            }
-        };
-    }
-}
+//package dev.sabri.securityjwt.config;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//public class CorsConfig {
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**") // Apply CORS to all endpoints
+//                        .allowedOrigins("http://localhost:3000") // Allow only your frontend origin
+//                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow necessary HTTP methods
+//                        .allowedHeaders("*") // Allow all headers
+//                        .allowCredentials(true); // Allow credentials (e.g., cookies, authorization headers)
+//            }
+//        };
+//    }
+//}
